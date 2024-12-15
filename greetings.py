60ebeca02260bfac09b4c7da1721788fa5c72f6f
olesechka02 <olesyatsygankova0902@gmail.com>
2024-12-16 02:58:20 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Олеся'
+print('Имя пользователя:', username)
+title = 'Домашка'
+print('Заголовок заметки:', title)
+content = 'История психологии'
+print('Описание заметки:', content)
+status = 'В работе'
+print('Статус:', status)
+created_date = '11-12-2024'
+print('Дата создания заметки:', created_date)
+issue_date = '30-12-2024'
+print('Дата истечения заметки:', issue_date)
