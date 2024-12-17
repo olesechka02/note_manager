07bd61a177a283aa6d253798cb6fc49b0290565c
olesechka02 <olesyatsygankova0902@gmail.com>
2024-12-17 22:12:43 +0700

    Update data_changer.py

--- data_changer.py
+++ data_changer.py
@@ -1,5 +1,8 @@
-import datetime
-dc = datetime.date(2024, 12, 11) # dc - это created_date
-print('Дата создания заметки:', dc.day,'-',dc.month)
-di = datetime.date(2024, 12, 30) #di - это issue_date
-print('Дата истечения заметки:', di.day,'-',di.month)
+from datetime import datetime
+created_date = input('Введите дату создания заметки в формате день-месяц-год: ')
+issue_date = input('Введите дату истечения срока заметки в формате день-месяц-год: ')
+created_date_obj = datetime.strptime(created_date, '%d-%m-%Y')
+issue_date_obj = datetime.strptime(issue_date, '%d-%m-%Y')
+
+print('Дата создания заметки:',created_date_obj.day,'-',created_date_obj.month)
+print('Дата истечения срока заметки:', issue_date_obj.day,'-',issue_date_obj.month)
